84913494e35dd1e7b29a16d1f27cdb3c435e32a9
Francisco <a00223609@mycambrian.ca>
2022-08-03 23:09:48 -0230

    #UserInfoContext

--- CalorieTracker/Models/UserInfoContext.cs
+++ CalorieTracker/Models/UserInfoContext.cs
@@ -1,6 +1,15 @@
-﻿namespace CalorieTracker.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CalorieTracker.Models
 {
-    public class UserInfoContext
+    public class UserInfoContext : DbContext
     {
+        public UserInfoContext(DbContextOptions<UserInfoContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<UserInfo> UserInfos { get; set; } = null!; 
     }
 }
