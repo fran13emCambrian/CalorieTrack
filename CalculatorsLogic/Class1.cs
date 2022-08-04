4ee86f7e867dd0d8fec76f62e1ee49c6d808c9ea
Francisco <a00223609@mycambrian.ca>
2022-08-03 22:32:51 -0230

    #CalorieCalculatorController

--- CalculatorsLogic/Class1.cs
+++ CalculatorsLogic/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CalculatorsLogic
 {
-    internal class Class1
+    public class Class1
     {
         //Calculate Ideal Weight
         //Values Height = cm 
--- CalorieTracker/Controllers/CalorieCalculator.cs
+++ CalorieTracker/Controllers/CalorieCalculator.cs
@@ -1,7 +1,21 @@
-﻿namespace CalorieTracker.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorsLogic;
+using Microsoft.AspNetCore.Mvc; 
+
+namespace CalorieTracker.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class CalorieCalculator
     {
-
-    }
+        //Takes data to calculate Calorie Diary Intake
+        [HttpGet]
+        public double CalculateCalorieIntake([FromQuery] Double height, [FromQuery] Double actualWeight, [FromQuery] Double age)
+        {
+            return Class1.CalorieIntake(height, actualWeight, age);
+        }
+    } 
 }
