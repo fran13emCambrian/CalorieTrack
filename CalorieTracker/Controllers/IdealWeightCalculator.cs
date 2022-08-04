a87a17f16612e8f63559a2e79e6818d48b945232
Francisco <a00223609@mycambrian.ca>
2022-08-03 22:48:16 -0230

    #CalculateIDealWeightController

---
+++ CalorieTracker/Controllers/IdealWeightCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorsLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieTracker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdealWeightCalculator : Controller
+    {
+            //Takes data to calculate Ideal Weight
+            [HttpGet]
+            public double CalculateIdealWeight([FromQuery] Double height)
+            {
+                return Class1.IdealWeight(height);
+            }
+        
+    }
+}
