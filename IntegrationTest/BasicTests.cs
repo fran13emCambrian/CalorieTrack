d5bee058480aee0c83a7c44d46c22db7904cdd50
Francisco <a00223609@mycambrian.ca>
2022-08-13 20:42:07 -0230

    //HomeControllerTest

---
+++ IntegrationTest/BasicTests.cs
@@ -0,0 +1,40 @@
+﻿using CalorieAppTrack;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTest
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+        //HomeControllerTests
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Home")]
+        [InlineData("/Home/Index")]
+        [InlineData("/Home/Privacy")]
+        [InlineData("/Home/Error")]
+        public async Task GetHttpRequest(string url)
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+
+            //Act 
+            var response = await client.GetAsync(url);  
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
---
+++ IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using CalorieAppTrack;
+using java.sql;
+using Xunit;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1 : IClassFixture<>
+    {
+       
+
+    }
+}
