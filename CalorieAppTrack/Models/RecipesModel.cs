607b1c466c45d94b5fc6306e16fca86c1defa851
Francisco <a00223609@mycambrian.ca>
2022-08-13 21:40:38 -0230

    #RecipesModel

---
+++ CalorieAppTrack/Models/RecipesModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CalorieAppTrack.Models
+{
+    public class RecipesModel
+    {
+        public int Id { get; set; }
+        public string RecipeName { get; set; }
+
+        public string RecipeDescription { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string PreparationTime { get; set; }
+
+    }
+}
